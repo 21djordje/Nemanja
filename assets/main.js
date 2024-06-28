4d897053de11509fb509618fdd476d9d518ddf4e
djordje.sinikovic <djordjesinikovic@gmail.com>
2024-06-28 19:33:14 +0200

    galerija :D

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var swiper = new Swiper(".swiper-container", {
-    loop: true, // Omogućava beskonačnu petlju
-    autoplay: {
-      delay: 5000, // Automatsko klizanje svakih 5 sekundi
-      disableOnInteraction: false, // Omogućava autoplay nakon interakcije korisnika
-    },
-    pagination: {
-      el: ".swiper-pagination", // Element za paginaciju
-      clickable: true, // Omogućava klik na paginaciju
-    },
-  });
-});
-
 window.addEventListener("scroll", function () {
   let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
   let navbar = document.querySelector("nav");
