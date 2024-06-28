63ef2abf1520834f2dfb10cca1a1d071a97ecaa3
21djordje <59032784+21djordje@users.noreply.github.com>
2024-06-28 11:28:43 +0200

    update

--- assets/main.js
+++ assets/main.js
@@ -11,3 +11,14 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   });
 });
+
+window.addEventListener("scroll", function () {
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+  let navbar = document.querySelector("nav");
+
+  if (currentScroll > 0) {
+    navbar.classList.add("menu-scrolled");
+  } else {
+    navbar.classList.remove("menu-scrolled");
+  }
+});
