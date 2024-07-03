0af81daa202cd4cf008028e8e442134dae05d85a
21djordje <59032784+21djordje@users.noreply.github.com>
2024-07-03 13:50:40 +0200

    js update

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,4 @@
+// Dodavanje klase .scroll na navbar kada se skroluje
 window.addEventListener("scroll", function () {
   let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
   let navbar = document.querySelector("nav");
@@ -6,5 +7,14 @@ window.addEventListener("scroll", function () {
     navbar.classList.add("menu-scrolled");
   } else {
     navbar.classList.remove("menu-scrolled");
+  }
+});
+
+// Provjera da li svaka ima alt atribut
+let images = document.querySelectorAll("img");
+
+images.forEach((img) => {
+  if (!img.alt || img.alt.trim() === "") {
+    img.alt = "Tremolo band za svadbe, rodjendane, krštenja i ostale proslave";
   }
 });
